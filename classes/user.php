78a8e626df05bb05d62e83bb8b02eee9211e5b96
Elogeek <elodiechristin@gmail.com>
2021-02-10 15:58:13 +0100

    les interfaces php exo 185

--- classes/user.php
+++ classes/user.php
@@ -2,16 +2,23 @@
 
 require('interfaces/UserInterface.php');
 
-class user implements UserInterface
-{
+class user implements UserInterface {
 
     private $request;
 
     public function __construct() {
         $this->getRequest($_REQUEST);
+
     }
 
 
+    public function getRequest($request)
+    {
+        // TODO: Implement getRequest() method.
+    }
 
-
+    public function parseRequest()
+    {
+        // TODO: Implement parseRequest() method.
+    }
 }
